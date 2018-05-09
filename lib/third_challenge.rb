bca34da8c804bb784a44b9fd9a7168c82ea22bf7
Grace Shih <vocalise@gmail.com>
2018-05-09 23:51:59 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,19 +3,21 @@ def third_challenge
    :montague => {
       :patriarch => {
         :name => "Lord Montague",
-        :age => 53
+        :age => "53"
       },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague"
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo"
+        :age => "15"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        :name => 
+
         },
       :matriarch => {
         #your key/value pairs here
