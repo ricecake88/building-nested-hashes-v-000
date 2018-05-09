fd029d5e1404ffccf1035ba66b0eda7210d159fe
Grace Shih <vocalise@gmail.com>
2018-05-09 23:49:01 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -15,8 +15,21 @@ def second_challenge
       }
     }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {
+        
+      },
+      :matriarch => {
+        
+      },
+      :hero => {
+        
+      }
+      :hero_friends => {
+        
+      }
+    }
+    
+  }
 
   
 end
