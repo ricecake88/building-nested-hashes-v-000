34ec6d1a1b0800e4048da70dcf488eba4ae93dc9
Grace Shih <vocalise@gmail.com>
2018-05-10 00:02:13 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -29,3 +29,6 @@ puts epic_tragedy[:capulet][:heroine][:status]
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
+
+epic = bonus
+puts(epic)
